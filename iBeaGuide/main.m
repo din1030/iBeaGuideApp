62e6c144d3e028e2653d58833bea9045a80fb69f
din1030 <din1030@gmail.com>
2015-10-26 19:29:28 +0800

    Initial Commit

---
+++ iBeaGuide/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iBeaGuide
+//
+//  Created by din1030 on 2015/10/26.
+//  Copyright © 2015年 Cheng Chia Ting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
